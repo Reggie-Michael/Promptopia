e2ce634f77b3ed144bcd4e252cd122b15131e9d0
Reggie-Michael <michaelrere82@gmail.com>
2024-01-08 19:57:34 +0000

    Fix typo

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import PromptCard from "./PromptCard";
 
-const PromptCardLsit = ({ data, handleTagClick }) => {
+const PromptCardList = ({ data, handleTagClick }) => {
   return (
     <div className="mt-16 prompt_layout">
       {data.map((post) => {
